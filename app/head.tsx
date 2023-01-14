af2748c4190b40552fde41d8194cc5e60d1a3b1f
harleydica <taufiklah87@gmail.com>
2023-01-14 13:50:28 +0700

    change title

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
   return (
     <>
-      <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
+      <title>Links — Taufik Crisnawan Santosa</title>
+      <meta name="description" content="Linktree + Next.js + Tailwind CSS" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
